b3f0690edabbbb42410cd0414ba78c409eaf410f
ilnur <leyman0111@gmail.com>
2020-10-26 16:48:46 +0300

    project

---
+++ src/main/java/ru/fmeter/manual/PassporterApplication.java
@@ -0,0 +1,13 @@
+package ru.fmeter.manual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fmeter/manual/config/PassporterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fmeter.manual.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
